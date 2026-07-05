7c18bff4986c3ff315d65d94829485165693e143
Davn <572909528@qq.com>
2026-07-05 22:41:57 +0800

    Add config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+window.WOJIAYOUPU_CONFIG = Object.freeze({
+  production: true,
+  supabase: Object.freeze({
+    url: "https://acmwhqjfejaabqhlqfte.supabase.co",
+    publishableKey: "sb_publishable_IudmNCOxQGCtxj5REJFHGg_jWGt6sLW"
+  })
+});
